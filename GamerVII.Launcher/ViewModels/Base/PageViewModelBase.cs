aed44090212a9dadf0930ec7f19c2b161acd9303
Anton Terentev <gamervii@icloud.com>
2023-10-29 14:13:17 +0300

    Initial commit

---
+++ GamerVII.Launcher/ViewModels/Base/PageViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿namespace GamerVII.Launcher.ViewModels.Base;
+
+/// <summary>
+/// Base class for view models related to pages.
+/// </summary>
+public abstract class PageViewModelBase : ViewModelBase
+{
+    
+}
---
+++ GamerVII.Launcher/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+
+namespace GamerVII.Launcher.ViewModels.Base;
+
+/// <summary>
+/// Base class for view models, derived from ReactiveObject.
+/// </summary>
+public class ViewModelBase : ReactiveObject
+{
+    
+}
