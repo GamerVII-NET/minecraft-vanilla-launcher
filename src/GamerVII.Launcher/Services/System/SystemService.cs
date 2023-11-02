b3e3ae9fdbc7a0d4fce34c332f174dedad2eae30
Anton Terentev <gamervii.phone@gmail.com>
2023-11-02 18:40:14 +0300

    Fixed the error of getting the maximum available amount of RAM in Unix systems

--- src/GamerVII.Launcher/Services/System/SystemService.cs
+++ src/GamerVII.Launcher/Services/System/SystemService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Mono.Unix;
 
 namespace GamerVII.Launcher.Services.System;
 
@@ -11,12 +13,43 @@ public class SystemService : ISystemService
 
     public ulong GetMaxAvailableRam()
     {
-        if (GetPhysicallyInstalledSystemMemory(out var totalMemoryInKb))
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            return totalMemoryInKb / 1024;
+            if (GetPhysicallyInstalledSystemMemory(out var totalMemoryInKb))
+            {
+                return totalMemoryInKb / 1024;
+            }
+        }else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return Convert.ToUInt64(GetUnixAvailableRam());
         }
+
         return 0;
     }
 
 
+    private ulong GetUnixAvailableRam()
+    {
+        string? output;
+
+        var info = new ProcessStartInfo("free -m")
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"free -m\"",
+            RedirectStandardOutput = true
+        };
+
+        using(var process = Process.Start(info))
+        {
+            output = process?.StandardOutput.ReadToEnd();
+        }
+
+        if (string.IsNullOrEmpty(output))
+            return 0;
+
+        var lines = output.Split("\n");
+        var memory = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        return Convert.ToUInt64(memory[1]);
+    }
 }
